680d2e5a3e3cc8dd08f9dbebb4b76619a3f93e30
felix eugenio mavila <felixmacarenco@gmail.com>
2025-01-09 23:10:43 +0100

    chore: create lambda functions

---
+++ EcommerceAws/lambda/products/productFechFunction.ts
@@ -0,0 +1,27 @@
+import { Method } from "aws-cdk-lib/aws-apigateway";
+import { APIGatewayProxyEvent, APIGatewayProxyResult , Context} from "aws-lambda";
+
+export async function handler(event:APIGatewayProxyEvent, context:Context):Promise<APIGatewayProxyResult> {
+
+    const method = event.httpMethod
+
+    if(event.resource === '/products'){
+       if(method === 'GET'){
+           console.log('GET')
+
+           return{
+            statusCode:200,
+            body:JSON.stringify({
+                message:'GET products - OK'
+            })
+           }
+       }
+    }
+
+    return {
+        statusCode:404,
+        body:JSON.stringify({
+            message:'Bad  Request'
+        })
+    }
+}
